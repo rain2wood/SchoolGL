a8cd0055ffe877b005fd40fdc092b060e2dacabb
Wiley Lau <wileylau@gmail.com>
2023-05-25 11:29:53 +0800

    SchoolGL: index: fixup all text
    * mirror all text from Google Login page
    
    Signed-off-by: Wiley Lau <wileylau@gmail.com>

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="style.css">
-    <title>Gmail</title>
+    <title>Sign in - Google Accounts</title>
     <style>
         /* Override "Email or Phone" text */
         .inputLabel:before {
-            content: "Email or Phone";
+            content: "Email or phone";
         }
     </style>
 </head>
@@ -19,14 +19,14 @@
 <body>
     <div class="inputContainer">
         <img src="google.png" alt="” width=”87" height="33">
-        <h2 class=”login“>Login</h2>
+        <h2 class=”login“>Sign in</h2>
         <h4 class="use">Use your Google Account</h4>
         <input type="email" class="gInput">
         <label class="inputLabel"></label>
         <div class="forgotContainer">
             <h5 class="forgot">Forgot Email?</h5>
         </div>
-        <h4 class="guest"> Not your computer? Use guest mode to sign in privately.<span> Learn More </span></h4>
+        <h4 class="guest">Not your computer? Use Guest mode to sign in privately.<span>Learn more</span></h4>
         <div class="buttonContainer">
             <p class="btnCreate">Create account</p>
             <p class="btnNext">Next</p>
